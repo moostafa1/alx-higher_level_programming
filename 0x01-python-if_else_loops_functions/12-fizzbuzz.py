128ee4a1b48e09dfdafea8226c2349403c7223e9
mostafa <desham8@gmail.com>
2023-08-08 16:35:20 -0700

    fixing logic

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
+    for i in range(1, 101):
         if not i % 3 and not i % 5:
             i = "FizzBuzz"
         elif not i % 3:
             i = "Fizz"
         elif not i % 5:
             i = "Buzz"
+
         if i != 100:
             print(i, end=" ")
         else:
