30bc85f9591cb0bb2cd8feb95fb46caf13082adf
mostafa <desham8@gmail.com>
2023-08-08 16:53:57 -0700

    prints the ASCII alphabet, in reverse orde

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+c = 1
+diff = 0
+for i in reversed(range(97, 123)):
+    print(chr(i - diff), end='')
+    c += 1
+    if not c % 2:
+        diff = 32
+    else:
+        diff = 0
+
