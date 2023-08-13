cacc31ac358aa018928a6d3fe275524b83301919
mostafa <desham8@gmail.com>
2023-08-13 08:05:01 -0700

    removing blank line

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_copy = my_list.copy()
     if idx not in range(len(my_list)):
         return my_list
+    list_copy = my_list.copy()
     list_copy[idx] = element
     return list_copy
-
