e9f0606ffa56730d99b3a275879798364cd5c1d2
mostafa <desham8@gmail.com>
2023-08-13 14:57:18 -0700

    fixing code style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,5 +13,8 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        print("{} {} {} = {}".format(argv[0], argv[1], argv[2],
-              sign_func[argv[1]](int(argv[0]), int(argv[2]))))
+        print("{} {} {} = {}".format(argv[0],
+                                     argv[1],
+                                     argv[2],
+                                     sign_func[argv[1]](int(argv[0]),
+                                     int(argv[2]))))
