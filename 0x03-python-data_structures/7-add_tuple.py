3ff8af25d7e3acd64458d2a90f97d5870a113bab
mostafa <desham8@gmail.com>
2023-08-15 05:31:20 -0700

    improving code style

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,9 +10,6 @@ def check_tuple_len(tup):
                 break
     return tuple(tup)
 
-
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
     a_plus_b = tuple([check_tuple_len(tuple_a)[i] +
         check_tuple_len(tuple_b)[i]
