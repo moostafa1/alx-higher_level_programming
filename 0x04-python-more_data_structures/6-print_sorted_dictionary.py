3566ca9ec3d111b022ce87f2d71ca342f58c00b1
mostafa <desham8@gmail.com>
2023-08-18 16:33:25 -0700

    fixing code logic

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_keys = sorted(list(a_dictionary))
-    sorted_dict = {k:a_dictionary[k] for k in dict_keys}
-    return sorted_dict
+    for k in a_dictionary: 
+        print("{:s}: {}".format(k, a_dictionary[k]))
