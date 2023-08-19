ad805456d954c57b0650457309a1c3f8109f9bb1
mostafa <desham8@gmail.com>
2023-08-18 17:12:37 -0700

    fixing code logic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
-        return max(a_dictionary.keys())
+        return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
     else:
         return None
