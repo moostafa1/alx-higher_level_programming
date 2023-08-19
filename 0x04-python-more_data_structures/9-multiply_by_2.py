11734ac0cbb4245d9293890d7538e5d8ac6564a0
mostafa <desham8@gmail.com>
2023-08-18 17:02:12 -0700

    fixing code style

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {k:a_dictionary[k] * 2 for k in a_dictionary}
+    new_dict = {k: a_dictionary[k]*2 for k in a_dictionary}
     return new_dict
