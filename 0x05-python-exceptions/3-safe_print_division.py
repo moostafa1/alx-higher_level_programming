8f9da0ca40d1c4e90f84c52e11d4f460d3d319e8
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 17:40:08 +0200

    improving logic

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,6 +3,7 @@
 def safe_print_division(a, b):
     try:
         print("Inside result: {:.1f}".format(a / b))
-        print("{:d} / {:d} = {:.1f}".format(a, b, a / b))
+        return a / b
     except ZeroDivisionError:
         print("Inside result: None")
+        return None
