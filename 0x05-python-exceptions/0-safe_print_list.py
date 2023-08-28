f5f15eb7bb6fcbbc904029f9b4df370feabb0946
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 16:58:21 +0200

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         cnt = 0
         for i in my_list:
-           cnt += 1
+            cnt += 1
         if x > cnt:
             x = cnt
         string = ''.join([str(v) for i, v in enumerate(my_list) if i < x])
         print("{}".format(string))
         return x
-    except:
+    except ValueError:
         print("\aPlease check that x is positive integer")
