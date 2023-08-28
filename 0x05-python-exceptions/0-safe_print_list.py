7c55f1437c6a493a845e241ec2b1a45d7466df12
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 16:52:40 +0200

    prints a list of x elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         cnt = 0
         for i in my_list:
-            cnt += 1
+           cnt += 1
         if x > cnt:
             x = cnt
         string = ''.join([str(v) for i, v in enumerate(my_list) if i < x])
