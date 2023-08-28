46055d322dfd91b02d205f42e95e4c718055eb1f
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 17:52:53 +0200

    adding finally block

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,10 @@
 
 def safe_print_division(a, b):
     try:
-        print("Inside result: ", end="")
-        print("{}".format(a / b))
-        return a / b
+        result = a / b
     except ZeroDivisionError:
-        print("None")
-        return None
+        result = None
+    finally:
+        print("Inside result: ", end="")
+        print("{}".format(result))
+    return result
