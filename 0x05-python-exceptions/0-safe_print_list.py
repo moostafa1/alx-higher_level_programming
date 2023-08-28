6b7c22104e258e58b78015f07e4d02ca05e111e0
root <root@DESKTOP-K1NJQMC.localdomain>
2023-08-28 16:50:52 +0200

    prints a list of x elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        cnt = 0
+        for i in my_list:
+            cnt += 1
+        if x > cnt:
+            x = cnt
+        string = ''.join([str(v) for i, v in enumerate(my_list) if i < x])
+        print("{}".format(string))
+        return x
+    except:
+        print("\aPlease check that x is positive integer")
