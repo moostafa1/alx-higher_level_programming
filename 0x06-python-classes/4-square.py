6d81264e7f799c601487783e5139cc394f03dc7a
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-05 01:04:53 +0200

    adding setter and getter to the code

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""defines a class called Square"""
+
+
+class Square:
+    """Attributes:
+        size: private (optional)
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__init__(value)
+
+    def area(self):
+        return self.__size ** 2
