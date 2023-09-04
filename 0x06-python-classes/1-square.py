28ecb312bd48023e23258654e08dba4af0b05057
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-05 00:13:37 +0200

    square class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""defines a class called Square"""
+
+
+class Square:
+    """Attributes:
+        size: private
+    """
+    def __init__(self, size):
+        self.__size = size
