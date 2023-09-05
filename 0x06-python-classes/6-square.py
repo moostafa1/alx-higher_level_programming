b929fd833d6f61a291083267b6f3faf17a294ed8
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-05 02:08:52 +0200

    adding position to the class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""defines a class called Square"""
+
+
+class Square:
+    """Attributes:
+        size: private (optional)
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print(self.__position[0]*"_", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
