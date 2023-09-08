a5e108f708a3dc0a6c205bed93f7534d3ec81a0e
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-08 22:42:15 +0200

    improving code logic

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,40 +1,18 @@
->>> text_indentation = __import__('5-text_indentation').text_indentation
+"""
+Defines a function that prints My name is <first name> <last name>
+"""
 
 
->>> text_indentation("Holberton School")
-Holberton School>>>
-
-
->>> text_indentation("Holberton.School")
-Holberton
-
-School>>>
-
-
->>> text_indentation("Holberton. School? How are you: John")
-Holberton
-
-School
-
-How are you
-
-John>>>
-
-
->>> text_indentation(12)
-Traceback (most recent call last):
-TypeError: text must be a string
-
-
->>> text_indentation("             ")
-             >>>
-
-
->>> text_indentation("\n")
-
->>>
-
-
->>> text_indentation()
-Traceback (most recent call last):
-TypeError: text_indentation() missing 1 required positional argument: 'text'
+def text_indentation(text):
+    """
+    Args:
+    first_name - first input name
+    last_name - last input name (optional)
+    Return: None
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    rem = [". ", "? ", ": ", ".", "?", ":"]
+    for i in rem:
+        text = text.replace(i, "\n\n")
+    print(text, end="")
