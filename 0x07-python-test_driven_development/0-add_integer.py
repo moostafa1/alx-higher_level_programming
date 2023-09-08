3b39db917cd760cf62005478eb58e22d4bf0968c
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-08 14:13:07 +0200

    1st task fixing logic

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
 """
+
 Defines an integer addition function
+
 """
 
 
 def add_integer(a, b=98):
     """Args:
-    a - integer or float
-    b - integer or float
-
-    Return: sum of a + b
-    """
+    a, b - integer or float
+    Return: sum of a + b"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
