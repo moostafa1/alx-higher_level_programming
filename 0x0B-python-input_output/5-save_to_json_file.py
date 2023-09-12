e27d300b346bc1299c271ef4979922f00cd6f195
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-12 16:09:37 +0200

    save to json file using with

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,7 +10,7 @@ def save_to_json_file(my_obj, filename):
     writes an Object to a text file, using a JSON representation
     """
     import json
-    from pathlib import Path
 
-    data = json.dumps(my_obj)
-    Path(filename).write_text(data)
+    # data = json.dumps(my_obj)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
