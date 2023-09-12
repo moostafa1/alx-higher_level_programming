49533526d97d39ac371ed6302d329ec7dbb87004
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-12 02:05:22 +0200

    class method that raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Defines an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
