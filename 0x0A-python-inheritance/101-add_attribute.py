22d0904f68d6b0973157ffdaa791c6b3da316040
root <root@DESKTOP-K1NJQMC.localdomain>
2023-09-12 04:04:00 +0200

    adding attribute to the function

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Defines a function function that adds a new attribute to an
+object if it’s possible
+"""
+
+
+def add_attribute(obj, attr, val):
+    """
+    adds new attribute to an object if it's possible
+    """
+    if hasattr(obj, attr):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, val)
