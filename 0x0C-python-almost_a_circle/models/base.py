163a18cba6903e50dbf9c1f1f970dbec0d69e6e1
Mostafa <desham8@gmail.com>
2023-12-10 01:12:39 +0200

    improvement: adding documentation to the constructor

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 """
-defines a Base class
-
-- if id is not None, assign the public instance attribute id with this
-  argument value - you can assume id is an integer and you don’t need to
-  test the type of it
-
-- otherwise, increment __nb_objects and assign the new value to the public
-  instance attribute id
+defines a Base class that should be the parent of all other created
+classes in this code
 """
 
 
@@ -16,6 +10,15 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        it checks:
+        - if id is not None, assign the public instance attribute id with this
+          argument value - you can assume id is an integer and you don’t need
+          to test the type of it
+
+        - otherwise, increment __nb_objects and assign the new value to the
+          public instance attribute id
+        """
         if id is not None:
             self.id = id
         else:
