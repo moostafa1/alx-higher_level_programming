a9318d0e9e365dc6457a4afd157a396e49074573
Mostafa <desham8@gmail.com>
2024-01-01 23:21:49 +0200

    fix: use SHOW CREATE instead of SHOW COLUMNS

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
 --  prints the full description of the table from the database
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+-- SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table FROM hbtn_0c_0;
