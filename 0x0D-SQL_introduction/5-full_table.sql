5e685ce31e097087763e4e8a8fac33cc8afc54c2
Mostafa <desham8@gmail.com>
2024-01-01 23:23:34 +0200

    fixing: remove FROM and fixing logic

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 --  prints the full description of the table from the database
 -- SHOW COLUMNS FROM hbtn_0c_0.first_table;
-SHOW CREATE TABLE first_table FROM hbtn_0c_0;
+SHOW CREATE TABLE first_table.hbtn_0c_0;
