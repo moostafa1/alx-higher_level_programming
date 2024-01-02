7f0a275a64d9a00cacad171435b2f6369b8e5e01
Mostafa <desham8@gmail.com>
2024-01-02 03:11:54 +0200

    fix: let VARCHAR(256) instead of 255

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,9 @@
 -- Step 1: Convert the database to utf8mb4
+-- Step 2: Convert the table to utf8mb4
+-- Step 3: Convert the field in the table to utf8mb4
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Step 2: Convert the table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Step 3: Convert the field in the table to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
