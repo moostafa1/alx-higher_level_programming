a7a232eb571c1b9e46a6fb4c6a9878ce393e401b
Mostafa <desham8@gmail.com>
2024-01-10 07:00:31 +0200

    fixing code logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 
 if (args.length > 1) {
-  args.sort();
+  args.sort((a, b) => parseInt(a) - parseInt(b));
   console.log(args[args.length - 2]);
 } else {
   console.log(0);
